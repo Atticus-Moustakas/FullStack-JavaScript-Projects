2205f8b61c6a0465eb0f00733c81a2965d076ade
Atticus Moustakas <atticus.moustakas@gmail.com>
2026-01-29 21:59:36 -0500

    tic-tac-toe done

--- tic-tac-toe-starter/script.js
+++ tic-tac-toe-starter/script.js
@@ -15,27 +15,90 @@ let gameIsOver = false;
 
 // The main event listener and the controller for the game
 function game(event) {
-  if (gameIsOver) return;
+  if (gameIsOver)
+    return window.alert(
+      "This game has ended. Refresh the page for a new game!"
+    );
 
   const cell = event.target;
+  const index = cell.id;
 
-  if (cell.classList.contains("o") || cell.classList.contains("x")) {
-    return;
-  }
-  
+  if (board[index]) return window.alert("This cell has already been marked!");
+
+  board[index] = markers[player];
   cell.classList.add(markers[player]);
+  numEmptyCells--;
+
+  updateGameStatus();
   switchPlayer();
-  
 }
 
 // --- Helper Methods ---
 
-// Return the linear index corresponding to the row and column subscripts
 function toLinearIndex(row, col) {
   return row * numRows + col;
 }
 
-// TODO: Add more!
 function switchPlayer() {
-  player = 1 - player; //switches player
+  player = player === 0 ? 1 : 0;
+}
+
+function updateGameStatus() {
+  if (checkRows() || checkColumns() || checkDiagonals()) {
+    window.alert(`${markers[player]} won!`);
+    gameIsOver = true;
+  } else if (numEmptyCells === 0) {
+    window.alert(`It's a draw!`);
+    gameIsOver = true;
+  }
+}
+
+function checkRows() {
+  for (let row = 0; row < numRows; row++) {
+    if (checkRow(row)) return true;
+  }
+  return false;
+}
+
+function checkRow(row) {
+  for (let col = 0; col < numCols; col++) {
+    const index = toLinearIndex(row, col);
+    if (board[index] !== markers[player]) return false;
+  }
+  return true;
+}
+
+function checkColumns() {
+  for (let col = 0; col < numCols; col++) {
+    if (checkColumn(col)) return true;
+  }
+  return false;
+}
+
+function checkColumn(col) {
+  for (let row = 0; row < numRows; row++) {
+    const index = toLinearIndex(row, col);
+    if (board[index] !== markers[player]) return false;
+  }
+  return true;
+}
+
+function checkDiagonals() {
+  return checkMajorDiagonal() || checkMinorDiagonal();
+}
+
+function checkMajorDiagonal() {
+  for (let row = 0; row < numRows; row++) {
+    const index = toLinearIndex(row, row);
+    if (board[index] !== markers[player]) return false;
+  }
+  return true;
+}
+
+function checkMinorDiagonal() {
+  for (let row = 0; row < numRows; row++) {
+    const index = toLinearIndex(row, numCols - row - 1);
+    if (board[index] !== markers[player]) return false;
+  }
+  return true;
 }
