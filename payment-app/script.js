a16b55d5ca29e13cc7c95a56a5b012846b0271a8
Atticus Moustakas <atticus.moustakas@gmail.com>
2026-01-31 16:51:46 -0500

    payment-03: Implement Luhn algorithm for card validation

--- payment-app/script.js
+++ payment-app/script.js
@@ -17,10 +17,34 @@ function handleFormSubmit(event) {
         window.alert("Invalid CVV, must be 3 or 4 digits.");
         return;
     }
+
+    const cnumber = document.getElementById("cnumber").value;
+    if (!/^[0-9]{13,16}$/.test(cnumber) || !isValid(cnumber)) {
+        window.alert("Invalid card number!");
+        return;
+    }
     
     window.alert("Thanks for your payment!");
 }
 
 function isValid(cnumber) {
-    return true;
+    let arr = cnumber
+        .split("")
+        .reverse()
+        .map((element) => parseInt(element));
+    
+    const sum = arr.reduce(reducer, 0);
+
+    function reducer(accumulator, currentValue, currentIndex) {
+        currentIndex += 1;
+        if (currentIndex % 2 === 0) {
+            currentValue *= 2;
+            if (currentValue > 9) {
+                currentValue -= 9;
+            }
+        }
+        return accumulator + currentValue;
+    }
+
+    return sum % 10 === 0;
 }
