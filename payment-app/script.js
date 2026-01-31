05f72b872d647c788be6777ea291e8e0756425d6
Atticus Moustakas <atticus.moustakas@gmail.com>
2026-01-31 16:28:04 -0500

    payment-02: Add form validation for expiration date and CVV

--- payment-app/script.js
+++ payment-app/script.js
@@ -5,5 +5,22 @@ function handleFormSubmit(event) {
     event.preventDefault();
 
     const month = document.getElementById("month").value;
+    const year = document.getElementById("year").value;
+
+    if (new Date() > new Date(year, month - 1)) {
+        window.alert("Your card is expired");
+        return;
+    }
+
+    const cvv = document.getElementById("cvv").value;
+    if (!/^[0-9]{3,4}$/.test(cvv)) {
+        window.alert("Invalid CVV, must be 3 or 4 digits.");
+        return;
+    }
     
+    window.alert("Thanks for your payment!");
+}
+
+function isValid(cnumber) {
+    return true;
 }
